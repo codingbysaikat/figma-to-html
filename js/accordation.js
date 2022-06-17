a2733c971811b4a3bc8effb6e1d8b9cd4a8aa7f0
saikat mondal <62213201+codingbysaikat@users.noreply.github.com>
2022-06-18 01:18:23 +0600

    hello changes

---
+++ js/accordation.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+        $('.single-accordation').click(function(){
+            var panel =this.children[1];
+
+                $(panel).slideToggle("slow");
+
+            
+            
+
+        })
+
+
+  })
--- js/main.js
+++ js/main.js
@@ -7,5 +7,16 @@ $(document).ready(function(){
         prevArrow:'<button class="btn-s-prev slide-btn"><img src="images/icons/arrow-left.png" alt=""></button>',
         nextArrow:' <button class="btn-s-prev-next"><img src="images/icons/arrow-right-slide.png" alt=""></button>'
       });
-    
+
+
+
+
+
+
   });
+  
+
+
+
+
+ 
