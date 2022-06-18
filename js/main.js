ef0eff06178dd1d665e7397ddbfa7bee9269cec7
saikat mondal <62213201+codingbysaikat@users.noreply.github.com>
2022-06-18 15:01:55 +0600

    Hello developer

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
     $('.autoplay').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 2000,
+        dots: true,
+        useCSS:false,
         prevArrow:'<button class="btn-s-prev slide-btn"><img src="images/icons/arrow-left.png" alt=""></button>',
         nextArrow:' <button class="btn-s-prev-next"><img src="images/icons/arrow-right-slide.png" alt=""></button>'
       });
